28d189f36b4f6be7c71a3401557840d6f16d1e3b
N-Santiago <norbertosantiago82@gmail.com>
2020-05-02 14:31:42 -0800

    users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,24 @@
 class UsersController < ApplicationController 
+    
+    def "users/:id" do
+        erb :
+    end 
+    
+    get "users/signup" do
 
+    end 
+
+    post "users/signup" do
+
+    end 
+
+    get "logout" do
+
+    end
+    
+    delete "deactivate" do
+
+    end 
 
 
 end 
