2125e8ecdf9effa7d62fa349dfe819ae88b14dd5
Norberto Santiago <norbertosantiago82@gmail.com>
2021-10-12 09:10:40 -0500

    Fixed code

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,41 +4,41 @@ class SessionsController < ApplicationController
 		if logged_in?
 			redirect to '/reviews'
 		else
-		erb :"users/signup"
+			erb :"users/signup"
 		end         
-    end 
+	end 
 
-    post "/signup" do
+	post "/signup" do
 		@user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
-        if @user.save
+		if @user.save
 			session[:user_id] = @user.id
 			redirect "/reviews"
-		  else
+		 else
 			redirect "/signup"
-		  end
-    end
-    
+		 end
+	end
+
 	get "/login" do
 		if logged_in?
 			redirect to '/reviews'
 		else 
-		erb :"users/login"
+			erb :"users/login"
 		end 
 	end
 
-    post "/login" do
-	    @user = User.find_by(username: params[:username])
+	post "/login" do
+	        @user = User.find_by(username: params[:username])
 		if @user && @user.authenticate(params[:password])
 			session[:user_id] = @user.id
 			redirect "/reviews"
 		else
 			redirect "/login"
 		end
-  end
+	end
 
-get "/logout" do
-	session.clear
-	redirect "/"
-end
+	get "/logout" do
+		session.clear
+		redirect "/"
+	end
 
 end
