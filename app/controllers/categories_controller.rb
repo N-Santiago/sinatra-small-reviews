d9b48fca231ab9707979778517c7325a07e713bc
Norberto Santiago <norbertosantiago82@gmail.com>
2021-11-29 13:14:43 -0600

    Update formatting

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,13 @@
 class CategoriesController < ApplicationController
 
-get "/categories" do
-    @categories = Category.all
-    erb :"categories/index"
-end 
+    get "/categories" do
+        @categories = Category.all
+        erb :"categories/index"
+    end 
 
-get "/categories/:id" do
-    @category = Category.find(params[:id])
-    erb :"categories/show" 
-end 
+    get "/categories/:id" do
+        @category = Category.find(params[:id])
+        erb :"categories/show" 
+    end 
 
 end 
